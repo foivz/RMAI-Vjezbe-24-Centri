cba1f8e458da545df5866a74ab2cb1b2eebebd14
Lovro Posarić <lovro.posaric@gmail.com>
2024-03-20 13:28:38 +0100

    Stvoren adapter za prikaz fragmenata i postavljen na glavni activity

--- app/src/main/java/hr/foi/rmai/memento/MainActivity.kt
+++ app/src/main/java/hr/foi/rmai/memento/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import hr.foi.rmai.memento.adapters.MainPagerAdapter
 
 class MainActivity : AppCompatActivity() {
     lateinit var tabLayout : TabLayout
@@ -17,5 +18,12 @@ class MainActivity : AppCompatActivity() {
         tabLayout = findViewById(R.id.tabs)
         viewPager = findViewById(R.id.viewpager)
 
+        val mainPagerAdapter = MainPagerAdapter(supportFragmentManager, lifecycle)
+        viewPager.adapter = mainPagerAdapter
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.setText(mainPagerAdapter.fragmentItems[position].titleRes)
+            tab.setIcon(mainPagerAdapter.fragmentItems[position].iconRes)
+        }.attach()
     }
 }
