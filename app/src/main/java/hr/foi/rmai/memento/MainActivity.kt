aeb12d9e6dc34b146215c48161d91866114bca13
Lovro Posarić <lovro.posaric@gmail.com>
2024-03-20 13:28:06 +0100

    Na glavni activity postavljeni dijelovi potrebni za prikaz navigacije

--- app/src/main/java/hr/foi/rmai/memento/MainActivity.kt
+++ app/src/main/java/hr/foi/rmai/memento/MainActivity.kt
@@ -2,10 +2,20 @@ package hr.foi.rmai.memento
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+    lateinit var tabLayout : TabLayout
+    lateinit var viewPager : ViewPager2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tabLayout = findViewById(R.id.tabs)
+        viewPager = findViewById(R.id.viewpager)
+
     }
 }
