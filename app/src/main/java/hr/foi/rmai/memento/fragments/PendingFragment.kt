9e2f1fbcb8c02b6ed2e147e89a0cb7ca393074bc
Lovro Posarić <lovro.posaric@gmail.com>
2024-03-21 15:37:13 +0100

    Dodan prazan RecyclerView

--- app/src/main/java/hr/foi/rmai/memento/fragments/PendingFragment.kt
+++ app/src/main/java/hr/foi/rmai/memento/fragments/PendingFragment.kt
@@ -6,11 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import hr.foi.rmai.memento.R
 import hr.foi.rmai.memento.helpers.MockDataLoader
 
 class PendingFragment : Fragment() {
     private val mockTasks = MockDataLoader.getDemoData()
+    private lateinit var recyclerView : RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,6 +23,10 @@ class PendingFragment : Fragment() {
         }
 
         return inflater.inflate(R.layout.fragment_pending, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerView = view.findViewById(R.id.rv_pending_tasks)
     }
 }
 
