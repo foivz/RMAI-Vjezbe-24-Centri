7e5da0868bfa7feb6c3f99a4c8c0ece676444bd8
Lovro Posarić <lovro.posaric@gmail.com>
2024-04-05 13:38:50 +0200

    MockDataLoader prebačen na korištenje baze podataka

--- app/src/main/java/hr/foi/rmai/memento/helpers/MockDataLoader.kt
+++ app/src/main/java/hr/foi/rmai/memento/helpers/MockDataLoader.kt
@@ -1,24 +1,34 @@
 package hr.foi.rmai.memento.helpers
 
+import hr.foi.rmai.memento.database.TasksDatabase
 import hr.foi.rmai.memento.entities.Task
 import hr.foi.rmai.memento.entities.TaskCategory
 import java.util.Date
 
 object MockDataLoader {
-    fun getDemoCategories() : List<TaskCategory> = listOf(
-        TaskCategory(1, "EP", "#000000"),
-        TaskCategory(2, "SIS", "#FF0000"),
-        TaskCategory(3, "RMAI", "#000080"),
-        TaskCategory(4, "OS", "#CCCCCC")
-    )
+    fun loadMockData() {
+        val tasksDao = TasksDatabase.getInstance().getTasksDao()
+        val taskCategoriesDao = TasksDatabase.getInstance().getTaskCategoriesDao()
 
-    fun getDemoData() : MutableList<Task> {
-        val categories = getDemoCategories()
+        if (tasksDao.getAllTasks(false).isEmpty()
+            && tasksDao.getAllTasks(true).isEmpty()
+            && taskCategoriesDao.getAllCategories().isEmpty())
+        {
+            val categories = arrayOf(
+                TaskCategory(1, "EP", "#000000"),
+                TaskCategory(2, "SIS", "#FF0000"),
+                TaskCategory(3, "RMAI", "#000080"),
+                TaskCategory(4, "OS", "#CCCCCC")
+            )
+            taskCategoriesDao.insertCategory(*categories)
 
-        return mutableListOf(
-            Task(1, "Submit seminar paper", Date(), 1, false),
-            Task(2, "Prepare for excerises", Date(), 1, false),
-            Task(3, "Rally a project name", Date(), 2, false),
-            Task(4, "Connect to server", Date(), 3, false))
+            val dbCategories = taskCategoriesDao.getAllCategories()
+            val tasks = arrayOf(
+                Task(1, "Submit seminar paper", Date(), dbCategories[0].id, false),
+                Task(2, "Prepare for excerises", Date(), dbCategories[1].id, false),
+                Task(3, "Rally a project name", Date(), dbCategories[1].id, false),
+                Task(4, "Connect to server", Date(), dbCategories[2].id, false))
+            tasksDao.insertTask(*tasks)
+        }
     }
 }
