494c9edec46efd59f55cc1b011d520dfdf25967f
Lovro Posarić <lovro.posaric@gmail.com>
2024-04-18 15:44:15 +0200

    Implementiran prikaz obavijesti za pending tasks

--- app/src/main/java/hr/foi/rmai/memento/adapters/TasksAdapter.kt
+++ app/src/main/java/hr/foi/rmai/memento/adapters/TasksAdapter.kt
@@ -1,32 +1,61 @@
 package hr.foi.rmai.memento.adapters
 
 import android.app.AlertDialog
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.SurfaceView
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.graphics.toColorInt
 import androidx.recyclerview.widget.RecyclerView
 import hr.foi.rmai.memento.R
 import hr.foi.rmai.memento.database.TasksDatabase
 import hr.foi.rmai.memento.entities.Task
+import hr.foi.rmai.memento.services.TaskTimerService
 import java.text.SimpleDateFormat
+import java.util.Date
 
 class TasksAdapter(val tasksList : MutableList<Task>,
-                   val onTaskCompleted: ((taskId: Int) -> Unit)? = null
+                   val onTaskCompleted: ((taskId: Long) -> Unit)? = null
                    ) : RecyclerView.Adapter<TasksAdapter.TaskViewHolder>() {
     inner class TaskViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         private val taskName: TextView
         private val taskDueDate: TextView
         private val taskCategory: SurfaceView
+        private val taskTimer: ImageView
 
         private val sdf: SimpleDateFormat = SimpleDateFormat("dd.MM.yyyy HH:mm")
+        private var isTimerActive = false
 
         init {
             taskName = view.findViewById(R.id.tv_task_name)
             taskDueDate = view.findViewById(R.id.tv_task_due_date)
             taskCategory = view.findViewById(R.id.sv_task_category_color)
+            taskTimer = view.findViewById(R.id.iv_task_timer)
+
+            view.setOnClickListener {
+                val selectedTask = tasksList[adapterPosition]
+                if (Date() < selectedTask.dueDate) {
+                    val intent = Intent(view.context, TaskTimerService::class.java).apply {
+                        putExtra("task_id", selectedTask.id)
+                    }
+
+                    isTimerActive = !isTimerActive
+
+                    if (isTimerActive) {
+                        taskTimer.visibility = View.VISIBLE
+                    } else {
+                        intent.putExtra("cancel", true)
+                        taskTimer.visibility = View.GONE
+                    }
+
+                    view.context.startService(intent)
+                } else if (taskTimer.visibility == View.VISIBLE) {
+                    taskTimer.visibility = View.GONE
+                }
+            }
 
             view.setOnLongClickListener {
                 val currentTask = tasksList[adapterPosition]
--- app/src/main/java/hr/foi/rmai/memento/services/TaskTimerService.kt
+++ app/src/main/java/hr/foi/rmai/memento/services/TaskTimerService.kt
@@ -5,6 +5,7 @@ import android.app.Notification
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import hr.foi.rmai.memento.R
@@ -16,6 +17,8 @@ import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import java.util.Date
 
 class TaskTimerService : Service() {
@@ -23,11 +26,10 @@ class TaskTimerService : Service() {
     private var started: Boolean = false
     private val NOTIFICATION_ID = 10000
 
+    private val mutex = Mutex()
     private var scope: CoroutineScope? = null
 
-    override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
-    }
+    override fun onBind(intent: Intent): IBinder? = null
 
     @SuppressLint("ForegroundServiceType")
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -36,6 +38,8 @@ class TaskTimerService : Service() {
 
         TasksDatabase.buildInstance(applicationContext)
         val task = TasksDatabase.getInstance().getTasksDao().getTask(taskId)
+
+        Log.e("RMAI", taskId.toString())
 
         if (tasks.contains(task)) {
             if (isCancelled) {
@@ -64,20 +68,29 @@ class TaskTimerService : Service() {
     @SuppressLint("MissingPermission")
     private suspend fun displayNotifications() {
         val sb = StringBuilder()
+        var taskToBeDeleted: Task? = null
 
         while (tasks.isNotEmpty()) {
-            for (task in tasks) {
-                val remainingMilliseconds = task.dueDate.time - Date().time
+            mutex.withLock {
+                for (task in tasks) {
+                    val remainingMilliseconds = task.dueDate.time - Date().time
 
-                if (remainingMilliseconds <= 0) {
-                    tasks.remove(task)
-                } else {
-                    sb.appendLine(task.name + ": " + remainingMilliseconds)
+                    if (remainingMilliseconds <= 0) {
+                        taskToBeDeleted = task
+                    } else {
+                        sb.appendLine(task.name + ": " + remainingMilliseconds)
+                    }
                 }
+
+                NotificationManagerCompat.from(applicationContext)
+                    .notify(NOTIFICATION_ID, buildTimerNotification(sb.toString()))
             }
 
-            NotificationManagerCompat.from(applicationContext)
-                .notify(NOTIFICATION_ID, buildTimerNotification(sb.toString()))
+            if (taskToBeDeleted != null) {
+                mutex.withLock {
+                    tasks.remove(taskToBeDeleted)
+                }
+            }
 
             delay(1000)
         }
